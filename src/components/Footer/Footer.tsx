1a79fef804f1334023145e73f990b466260d4b00
MaxOvs19 <gshk3669@gmail.com>
2023-11-15 15:09:59 +0300

    added Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-top"></div>
+      <div className="footer-midl"></div>
+      <div className="footer-bottom"></div>
+    </footer>
+  );
+};
+
+export default Footer;
